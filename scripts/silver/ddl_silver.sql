96bc088a22304bd64160614c161b3644323532e7
Ulkar <109944640+UlkarHuseynova@users.noreply.github.com>
2026-05-17 16:44:42 +0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 EXEC silver.load_silver
 CREATE OR ALTER PROCEDURE silver.load_silver AS
 BEGIN
